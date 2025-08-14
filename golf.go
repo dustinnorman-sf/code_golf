e767214ed7dd3617951fa8c1a0d2c2e3efbff024
Dustin Norman <dustin.norman@progress.com>
2025-08-14 08:05:57 -0400

    first submission

---
+++ golf.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	. "bufio"
+	. "math"
+	. "os"
+	. "strconv"
+	st "strings"
+)
+
+func main() {
+	r := []string{"M", "C", "X", "I", "D", "L", "V"}
+	f, _ := Open(Args[1])
+	s := NewScanner(f)
+	for s.Scan() {
+		n, _ := Atoi(s.Text())
+		o := ""
+		for i := range 4 {
+			a := int(Pow10(3 - i))
+			c := n / a
+			x := r[i]
+			h := r[i+3]
+			if c == 4 {
+				o += x + h
+			} else if c == 9 {
+				o += x + r[i-1]
+			} else if c > 0 {
+				if c >= 5 {
+					o += h
+				}
+				o += st.Repeat(x, c%5)
+			}
+			n -= c * a
+		}
+		println(o)
+	}
+}
